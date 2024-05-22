5a0a74aa5007e141254d8de03a66b8582a56a1a6
zEleceed <133176949+zEleceed@users.noreply.github.com>
2024-05-22 09:43:54 +0900

    bmi calculator

--- hello_world.py
+++ hello_world.py
@@ -1,7 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def hello_world():
-    return render_template("hello_world.html")
+    if request.method == "POST":
+        try:
+            height = float(request.form["number1"])
+            weight = float(request.form["number2"])
+            bmi =  weight / (height*height)
+            result = f"Your BMI is : {bmi:.2f}"
+        except ValueError:
+            result = "Invalud input."
+    else:
+        result = None
+
+    return render_template("base.html", result=result)
+
+if __name__ == "__main__":
+    app.run(debug=True)
