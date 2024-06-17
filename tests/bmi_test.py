3a8e3f03af34c310c6a917265ff5425d480b0a81
zEleceed <133176949+zEleceed@users.noreply.github.com>
2024-06-17 11:12:55 +0900

    adjusted bmi_test.py for conftest.py

--- tests/bmi_test.py
+++ tests/bmi_test.py
@@ -1,17 +1,3 @@
-import pytest
-from app.app import app as flask_app
-
-
-@pytest.fixture
-def app():
-    yield flask_app
-
-
-@pytest.fixture
-def client(app):
-    return app.test_client()
-
-
 def test_bmi_calc_get(client):  #testing the GET method for /bmi route
     response = client.get('/bmi')
     assert response.status_code == 200
