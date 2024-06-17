ba47f4ea66d5612142572b7906bf142e31b24844
zEleceed <133176949+zEleceed@users.noreply.github.com>
2024-06-17 16:52:24 +0900

    - Added, GIVEN,WHEN,THEN examples for tests

--- tests/bmi_test.py
+++ tests/bmi_test.py
@@ -1,11 +1,21 @@
-def test_bmi_calc_get(client):  #testing the GET method for /bmi route
+def test_bmi_calc_get(client):
+    """
+       GIVEN a testing client
+       WHEN the '/bmi' endpoint is requested via GET
+       THEN check that the response is valid and contains the expected fields
+    """
     response = client.get('/bmi')
     assert response.status_code == 200
     assert b'Height' in response.data
     assert b'Weight' in response.data
 
 
-def test_bmi_calc_post(client):  #testing POST method
+def test_bmi_calc_post(client):
+    """
+    GIVEN a testing client
+    WHEN the '/bmi' endpoint is posted to with height and weight data
+    THEN check that the response gives correct calculations
+    """
     response = client.post("/bmi", data={"number1": '1.80', "number2": '60'})
     assert response.status_code == 200
     assert b'Your BMI is : 18.52' in response.data
