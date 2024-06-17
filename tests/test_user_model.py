7401f9544bf43139088701ae1800b22b25ed0d0f
zEleceed <133176949+zEleceed@users.noreply.github.com>
2024-06-17 16:36:04 +0900

    - Added more tests for test_user_model.py

--- tests/test_user_model.py
+++ tests/test_user_model.py
@@ -6,12 +6,28 @@ def test_new_user(new_user):
     """
         GIVEN a User model
         WHEN a new User is created
-        THEN check the username, name, and password_hash fields are defined correctly
+        THEN check the username, name, are defined correctly
     """
     assert new_user.username == "testuser"
     assert new_user.name == "Test User"
-    assert new_user._password_hash != "TestingIsGreat"  # Ensure the password is hashed
-    assert new_user.authenticate("TestingIsGreat")
 
 
+def test_new_user_hashing_password(new_user):
+    """
+        GIVEN a User model
+        WHEN the password is set
+        THEN check the password is hashed correctly and not stored as plaintext
+    """
+    new_user.password_hash = 'TestingIsGreat'
+    assert new_user._password_hash != 'TestingIsGreat'  # Ensure the password is hashed
 
+
+def test_user_authenticated(new_user):
+    """
+        GIVEN an existing user model
+        WHEN the password is set and authenticated
+        THEN check if the password is correct for the user
+    """
+    new_user.password_hash = "TestingIsAwesomeeeee"
+    assert new_user.authenticate("TestingIsAwesomeeeee")
+    assert not new_user.authenticate("Fake")
