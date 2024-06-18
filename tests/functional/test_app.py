cdd3d636774f57edc5a1289c8a63f40d9215bda4
zEleceed <133176949+zEleceed@users.noreply.github.com>
2024-06-18 19:25:33 +0900

    - Making login_attempt test

--- tests/functional/test_app.py
+++ tests/functional/test_app.py
@@ -22,3 +22,31 @@ def test_login_page(client):
     assert b"Login" in response.data
     assert b"Username" in response.data
     assert b"Password" in response.data
+
+
+def test_signup_page(client):
+    """
+    GIVEN a Flask application configured for testing
+    WHEN the '/signup' page is requested (GET)
+    THEN check that the response is valid
+    """
+    response = client.get("/signup")
+    assert response.status_code == 200
+    assert b"Name" in response.data
+    assert b"Username" in response.data
+    assert b"Password" in response.data
+    assert b"Signup" in response.data
+
+
+def test_login_attempt(client, init_database):
+    """
+     GIVEN a Flask application configured for testing
+     WHEN the '/login' page is posted to with invalid credentials (POST)
+     THEN check an error message is returned to the user
+     """
+    response = client.post("/login",
+                           data=dict(username="123123", password="123123123123"),
+                           follow_redirects=True)
+    assert response.code == 400
+
+
