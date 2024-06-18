659eb43edd18d5ffbe739b598275cf9ef03f85d7
zEleceed <133176949+zEleceed@users.noreply.github.com>
2024-06-18 18:12:28 +0900

    - Added test_login_page function.

--- tests/functional/test_app.py
+++ tests/functional/test_app.py
@@ -9,3 +9,16 @@ def test_home_page(client):
     assert b"Welcome to the BMI Calculator" in response.data
     assert b"Login" in response.data
     assert b"Sign Up" in response.data
+
+
+def test_login_page(client):
+    """
+    GIVEN a Flask application configured for testing
+    WHEN the '/login' page is requested (GET)
+    THEN check that the response is valid
+    """
+    response = client.get("/login")
+    assert response.status_code == 200
+    assert b"Login" in response.data
+    assert b"Username" in response.data
+    assert b"Password" in response.data
